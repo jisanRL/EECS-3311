daade1e1b1ec2cb8cb8099d7c1ea9f8315936b00
jisanRL <jisanlatif@gmail.com>
2021-03-25 18:17:41 -0400

    sturcture for homePage done, refurbish later, admin page next

--- eecs3311_proj/src/main/java/GUI/HomePage.java
+++ eecs3311_proj/src/main/java/GUI/HomePage.java
@@ -3,19 +3,32 @@ package GUI;
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 
 public class HomePage extends JFrame {
 	
-	private static JPanel contentPane;
-	private static JLabel thisLabel;
-
+	private JPanel contentPane;
+	private JLabel thisLabel;
+	private JLabel selectParkingSpot;
+	private JTextField timeSlotinput;
+	private JLabel timeAdded;
+	private JButton timeSlotButton;
+	private JLabel price;
+	
+	private JButton bookButton;
+	private JButton payButton;
+	private JButton viewBookingButton;
+	
 	/*
 	 *REQ-4.4 and 4.5 and 4.6 Booking Space/ Main Dashboard 
 	 */
@@ -32,7 +45,6 @@ public class HomePage extends JFrame {
 				}
 			}
 		});
-		
 	}
 	
 	public HomePage() {
@@ -79,14 +91,104 @@ public class HomePage extends JFrame {
 		contentPane.add(lst);
 		
 		
-		// table view of all the available spot of the choosen location
+		/* view of all the available spot of the choosen location */
+		//available slot
 		JLabel tag1 = new JLabel();
 		int count = 0;
 		tag1.setText("Available Slots: " + count);
 		tag1.setBounds(10, 125, 170, 25);
 		contentPane.add(tag1);
 		
+		// select parking slots
+		selectParkingSpot = new JLabel();
+		selectParkingSpot.setText("Select Spot: ");
+		selectParkingSpot.setBounds(10, 160, 170, 25);
+		contentPane.add(selectParkingSpot);
+		
+		// available time slot
+		JLabel timeSlot = new JLabel();
+		timeSlot.setText("Time Slot: ");
+		timeSlot.setBounds(10, 190, 170, 25);
+		contentPane.add(timeSlot);
+		
+		// timeSlot input
+		timeSlotinput = new JTextField(20);					// max length of input char = 20
+//		HintTextField textfield = new HintTextField("Enter your hint");   // later -> add a placeholder text in this input 
+		timeSlotinput.setBounds(100,190,170,25);				
+		contentPane.add(timeSlotinput);
+		
+//		JLabel tag2 = new JLabel("hours");   useless delete later 
+//		tag2.setBounds(270,150,170,25);
+//		contentPane.add(tag2);
+		
+		String[] timeArr = {"Hrs", "Min"};
+		JComboBox tag2 = new JComboBox(timeArr);
+		tag2.setSelectedIndex(1);
+		tag2.setBounds(265,192,80,25);
+		contentPane.add(tag2);
+		
+		// time-slot button    [may not be needed]
+		timeSlotButton = new JButton("Enter Time");
+	    // actionListener to confirm time
+		timeSlotButton.addActionListener(new ActionListener() {
+	    	 // time slot confimation
+			public void actionPerformed(ActionEvent e) {
+				String time = timeSlotinput.getText();
+				double doubleTime = Double.parseDouble(time);
+				
+				// set the time limit for parkinghere [assumtion: customer can book anytime between 0 to 24hrs]
+				// this one is for hours; later -> make the same arrangement for miniutes when cs chooses minitues
+				if (doubleTime > 0.0 && doubleTime < 24.0) {
+					System.out.println(doubleTime);
+					timeAdded.setText("Time added successfully");
+				} else {
+					timeAdded.setText("Invalid timing!! Please put time between 0 to 24 hrs");
+				}
+			}	    	
+	     });
+		timeSlotButton.setBounds(340,190,100,25);
+	    contentPane.add(timeSlotButton);
+		
+	    timeAdded = new JLabel("");
+	    timeAdded.setBounds(100,220,2000,25);
+	    contentPane.add(timeAdded);
+	    
+	    // price 
+	    price = new JLabel();
+	    price.setText("Price: " + " $time * 0.5");  // fix this later
+	    price.setBounds(10, 240, 170, 25);
+		contentPane.add(price);
+		
+		// confirm buttons [later add action listener]
+		bookButton = new JButton("Confirm Booking");
+		bookButton.setBounds(60, 260, 370, 25);
+	    contentPane.add(bookButton);
+	    
+	    // pay button  [later add action listener]
+		payButton = new JButton("Pay");
+		payButton.setBounds(60, 285, 370, 25);
+	    contentPane.add(payButton);
+	    
+	    // view booking button  [later add action listener]
+		viewBookingButton = new JButton("View Bookings");
+		viewBookingButton.setBounds(60, 310, 370, 25);
+		contentPane.add(viewBookingButton);
+		
 	}
 	
-
+	// price calculator per min [move it to the backend class later]
+	// time in min
+	private static String priceTag(double time) {
+		// per min -> $0.5
+		double price = 0.0;
+		// in terms of min
+		if (time > 0 && time < 60) {
+			price = time * 0.5;
+		} else if (time > 60 && time < 1440) {
+			double timehrs = time/60;			// convert min to hrs
+			price = timehrs * 30; 				//"calculate in terms of hrs later";
+		}
+		return "$" + price;
+	}
+	
 }
