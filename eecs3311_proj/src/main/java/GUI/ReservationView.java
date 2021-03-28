077b5884553bb944c0c2f0efc64478b8ff382193
jisanRL <jisanlatif@gmail.com>
2021-03-28 01:35:56 -0400

    6 Gui's reservation table left

--- eecs3311_proj/src/main/java/GUI/ReservationView.java
+++ eecs3311_proj/src/main/java/GUI/ReservationView.java
@@ -1,12 +1,21 @@
 package GUI;
 
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Font;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTabbedPane;
+import javax.swing.JTable;
 
-public class ReservationView {
+public class ReservationView extends JFrame {
 
-	private JFrame frame;
+	private JPanel contentPane;
+	private JLabel thisLabel;
 
 	/**
 	 * Launch the application.
@@ -16,7 +25,7 @@ public class ReservationView {
 			public void run() {
 				try {
 					ReservationView window = new ReservationView();
-					window.frame.setVisible(true);
+					window.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -28,16 +37,26 @@ public class ReservationView {
 	 * Create the application.
 	 */
 	public ReservationView() {
-		initialize();
-	}
+		// create the panel
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 550, 400); 							// x y len wid
+		contentPane = new JPanel();
+		contentPane.setForeground(new Color(0, 0, 0));
+		contentPane.setBackground(new Color(192, 192, 192)); 	// background color of the app -> Silver
+		contentPane.setBorder(new EmptyBorder(6, 6, 6, 6));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 550, 400);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// title
+		thisLabel = new JLabel("instaParking"); // Logo
+		thisLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		thisLabel.setFont(new Font("Sans-serif", Font.PLAIN, 16));
+		thisLabel.setBackground(Color.WHITE);
+		thisLabel.setBounds(140, 15, 170, 25); // set the position of the component
+		contentPane.add(thisLabel);
+		
+		// the table -> https://www.geeksforgeeks.org/java-swing-jtable/
+		
+		
 	}
-
 }
