b3003341c9dda876ff6061e2ac637f454eff31c9
jisanRL <jisanlatif@gmail.com>
2021-04-05 19:37:12 -0400

    push-1;admin operations opeational

---
+++ eecs3311_proj/src/main/java/AdminOperationsModel/Admin.java
@@ -0,0 +1,22 @@
+package AdminOperationsModel;
+
+/*
+ * REQ: 4.1 and 4.9
+ * 		4.1.3-REQ-1: The system must assign a unique ID to each new parking enforcement officer added
+ * 		4.1.3-REQ-1: The system must verify the parking enforcement officer exists in the system before removing an officer
+ * 		4.1.3-REQ-3: The system must verify a new parking enforcement officer’s ID does not exist in the system already
+ * 		4.1.3-REQ-4: The system must store a new parking enforcement officer’s registration information
+ * 		
+ * 		4.9.3-REQ-1: The user must be an systems administrator.
+ * 		4.9.3-REQ-2: The system must verify the customer’s existence before changing their payment status.
+ * 		4.9.3-REQ-3: The system must verify the customer’s occupancy of the parking space before changing their payment status
+ * 		4.9.3-REQ-4: The system must verify the customer’s payment of the parking space before changing their payment status
+ */
+public class Admin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Admin");
+	}
+
+}
